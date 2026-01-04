39f008221de4c8566d549082f9a268e039647e78
Spicy - musicenter <zerikarliodam@gmail.com>
2026-01-04 16:37:48 +0500

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <>
-      <a href={`${Homepage}/blob/main/LICENSE`} target="_blank"> NETLIVY TOMONIDAN YARATILDI</a>
+      <a> NETLIVY TOMONIDAN YARATILDI</a>
       <span>
         <span>NETLIVY NEWS © 2026</span>
         
