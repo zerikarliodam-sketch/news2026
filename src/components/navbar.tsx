0c35e104c684d535ab3b8d3cba6bccc17666bda5
Spicy - musicenter <zerikarliodam@gmail.com>
2026-01-04 16:34:56 +0500

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -19,20 +19,10 @@ export function NavBar() {
           "cursor-pointer transition-all",
         )}
       >
-        SEARCH/more..
+        Ko'proq
       </button>
       {fixedColumnIds.map(columnId => (
-        <Link
-          key={columnId}
-          to="/c/$column"
-          params={{ column: columnId }}
-          className={$(
-            "px-2 hover:(bg-primary/10 rounded-md) cursor-pointer transition-all",
-            currentId === columnId ? "color-primary font-bold" : "op-70 dark:op-90",
-          )}
-        >
-          {metadata[columnId].name}
-        </Link>
+       
       ))}
     </span>
   )
