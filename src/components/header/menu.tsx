788dbd0c0d26ab1bff49dd75d26a06434964a425
Spicy - musicenter <zerikarliodam@gmail.com>
2026-01-04 16:13:39 +0500

    Update menu.tsx

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -55,35 +55,35 @@ export function Menu() {
                 ? (
                     <li onClick={logout}>
                       <span className="i-ph:sign-out-duotone inline-block" />
-                      <span>退出登录</span>
+                      <span>Chiqish</span>
                     </li>
                   )
                 : (
                     <li onClick={login}>
                       <span className="i-ph:sign-in-duotone inline-block" />
-                      <span>Github 账号登录</span>
+                      <span>Login</span>
                     </li>
                   ))}
               {/* <ThemeToggle /> */}
               <li onClick={() => window.open(Homepage)} className="cursor-pointer [&_*]:cursor-pointer transition-all">
                 <span className="i-ph:github-logo-duotone inline-block" />
-                <span>Star on Github </span>
+                <span>Star </span>
               </li>
               <li className="flex gap-2 items-center">
                 <a
-                  href="https://github.com/ourongxing/newsnow"
+                  href="https://instagram.com/car1movvvv"
                 >
                   <img
-                    alt="GitHub stars badge"
-                    src="https://img.shields.io/github/stars/ourongxing/newsnow?logo=github&style=flat&labelColor=%235e3c40&color=%23614447"
+                    alt=" badge"
+                    src="https://instagram.com/car1movvvv"
                   />
                 </a>
                 <a
-                  href="https://github.com/ourongxing/newsnow/fork"
+                  href="https://instagram.com/car1movvvv"
                 >
                   <img
-                    alt="GitHub forks badge"
-                    src="https://img.shields.io/github/forks/ourongxing/newsnow?logo=github&style=flat&labelColor=%235e3c40&color=%23614447"
+                    alt="forks"
+                    src="https://instagram.com/car1movvvv"
                   />
                 </a>
               </li>
