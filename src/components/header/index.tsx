8cf66ebc8b1c9da132aab50a3bef76b219c99d23
Spicy - musicenter <zerikarliodam@gmail.com>
2026-01-05 02:23:21 +0500

    Update index.tsx

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -52,9 +52,9 @@ export function Header() {
         <Link to="/" className="flex gap-2 items-center">
           <div className="h-10 w-10 bg-cover" title="logo" style={{ backgroundImage: "url(/icon.svg)" }} />
           <span className="text-2xl font-brand line-height-none!">
-         
+         <p>NETLIVY </p>
             <p className="mt--1">
-              <span className="color-primary-6">NETLIVY</span>
+              <span className="color-primary-6"></span>
               <span></span>
             </p>
           </span>
